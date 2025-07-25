ff233e6403a1708cd04aad5851001b5ee4e679ad
Vishnu Mohan <vishnumohanmfc@gmail.com>
2025-07-25 22:01:50 +0530

    feat: added loading animation

--- client/src/components/ui/Loader.tsx
+++ client/src/components/ui/Loader.tsx
@@ -1,15 +1,12 @@
 const BarLoader = () => {
-  const bars = [0, 100, 200, 300, 400];
-
     return (
-        <div className="flex gap-1 items-end h-12">
-            {bars.map((delay, i) => (
-                <div
-                    key={i}
-                    className="w-2 h-4 bg-black animate-bar"
-                    style={{ animationDelay: `${delay}ms` }}
-                />
-            ))}
+        <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center z-50 bg-white">
+            <div className="flex items-center space-x-2 text-lg font-semibold text-gray-600">
+                <span>Loading</span>
+                <span className="animate-bounce delay-[0ms]">.</span>
+                <span className="animate-bounce delay-[150ms]">.</span>
+                <span className="animate-bounce delay-[300ms]">.</span>
+            </div>
         </div>
     );
 };
